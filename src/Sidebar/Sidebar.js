fc58749576b299ac541351ae9851800b23890bc4
yosef <yoseftutian@gmail.com>
2024-03-24 20:11:27 +0200

    Using categories in input components

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -2,19 +2,20 @@ import Category from "./Category/Category";
 import Price from "./Price/Price";
 import Colors from "./Colors/Colors";
 import "./Sidebar.css";
-function Sidebar() {
+
+const Sidebar = ({ handleChange }) => {
   return (
-    <div>
+    <>
       <section className="sidebar">
         <div className="logo-container">
           <h1>🛒</h1>
         </div>
-        <Category />
-        <Price />
-        <Colors />
+        <Category handleChange={handleChange} />
+        <Price handleChange={handleChange} />
+        <Colors handleChange={handleChange} />
       </section>
-    </div>
+    </>
   );
-}
+};
 
 export default Sidebar;
